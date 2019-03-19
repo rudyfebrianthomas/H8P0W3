a99edd162b6703a8455310e59bfe59da54ed4376
Rudy Febrian Thomas <RT37730X@cimbniaga.co.id>
2019-03-19 18:41:03 +0700

    tugas week3

--- modus.js
+++ modus.js
@@ -1,29 +1,29 @@
 function cariModus(arr) {
     // you can only write your code here!
-    var count = 0;
-  var modus;
-  for(var i = 0; i< arr.length; i++){
-    var newCount = 0;
-    for(var j = 0; j<arr.length;j++){
-      if(arr[i] === arr [j] && i !==j){
-        newCount++;
-        if(newCount>count){
-          count = newCount;
-          modus = arr[i];
+    var a=0;
+    var modus;
+    for(var i=0;i<arr.length;i++){
+      var b=0;
+      for (var j=0;j<arr.length;j++){
+        if (arr[i]===arr[j] && i!==j){
+          b++;
+          if (b>a){
+            a=b;
+            modus=arr[i];
+          }
         }
       }
     }
-  }
-  if(modus === undefined){
-    return -1;
-  } else if(newCount === arr.length-1){
-    return -1;
-  }
-  
-  return modus;
     
-  }
-  
+    if(modus===undefined){
+      return -1;
+    } else if(b===arr.length-1){
+      return -1;
+    }
+
+    return modus; 
+    }
+    
   // TEST CASES
   console.log(cariModus([10, 4, 5, 2, 4])); // 4
   console.log(cariModus([5, 10, 10, 6, 5])); // 5
